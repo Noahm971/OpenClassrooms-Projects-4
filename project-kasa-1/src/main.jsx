64f0f62ff1ee094ca5ca26330bd175b6f3276ffa
Noah Mennock <mennock.noah@gmail.com>
2026-07-21 16:32:06 +0200

    Installation React Router

--- project-kasa-1/src/main.jsx
+++ project-kasa-1/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
